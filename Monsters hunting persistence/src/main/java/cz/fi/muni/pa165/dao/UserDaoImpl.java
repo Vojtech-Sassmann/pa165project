7d710e079ecffa6981ab6d299c8c959eb53ecd83
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2017-10-24 15:42:09 +0200

    Created basic interfaces for dao
    * Created basic dao interface classes

--- Monsters hunting persistence/src/main/java/cz/fi/muni/pa165/dao/UserDaoImpl.java
+++ Monsters hunting persistence/src/main/java/cz/fi/muni/pa165/dao/UserDaoImpl.java
@@ -1,4 +1,30 @@
 package cz.fi.muni.pa165.dao;
 
-public class UserDaoImpl {
+import cz.fi.muni.pa165.entity.User;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.inject.Named;
+import java.util.List;
+
+@Named
+public class UserDaoImpl implements UserDao {
+	@Override
+	public void create(User user) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public void delete(User user) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public User findById(Long id) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public List<User> getAll() {
+		throw new NotImplementedException();
+	}
 }
